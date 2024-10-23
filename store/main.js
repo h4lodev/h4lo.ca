4034b519cd17004a068ea49db3fb3ddad45ebd0d
H4LO <75143373+0-H4LO-0@users.noreply.github.com>
2024-10-23 00:03:00 -0300

    correct modal popup

--- store/main.js
+++ store/main.js
@@ -1076,12 +1076,6 @@
 		})();
 	
 	// Icons: buttonfooter.
-		$('#buttonfooter > li:nth-child(2) > a').addEventListener(
-			'click',
-			function(event) { 
-				triggerModal()
-			}
-		);
 		
 		$('#buttonfooter > li:nth-child(3) > a').addEventListener(
 			'click',
