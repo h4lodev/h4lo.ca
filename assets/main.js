e9020c8774af976baa9b25dbfaeabf54533a95f6
H4LO <75143373+0-H4LO-0@users.noreply.github.com>
2024-12-02 14:45:01 -0400

    Attempt Lenis integration

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,15 @@
+import Lenis from 'lenis'
+
+// Initialize Lenis
+const lenis = new Lenis({
+	autoRaf: true,
+  });
+  
+  // Listen for the scroll event and log the event data
+  lenis.on('scroll', (e) => {
+	console.log(e);
+  });
+  
 (function() {
 
 	// Main.
