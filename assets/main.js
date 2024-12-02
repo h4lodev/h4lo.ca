164248fd4b88094669fa4e7a6353741cd5d58237
H4LO <75143373+0-H4LO-0@users.noreply.github.com>
2024-12-02 14:53:09 -0400

    give up

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,5 @@
 (function() {
-
 	// Main.
-		// Initialize Lenis
-		const lenis = new Lenis({
-		autoRaf: true,
-		});
-  
-		// Listen for the scroll event and log the event data
-  		lenis.on('scroll', (e) => {
-		console.log(e);
- 		});
-  
 		var	on = addEventListener,
 			off = removeEventListener,
 			$ = function(q) { return document.querySelector(q) },
